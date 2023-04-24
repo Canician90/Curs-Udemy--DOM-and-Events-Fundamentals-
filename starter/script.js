f6bfedb3acec6c187a4017893ce324bf450e1aaa
Canician90 <127733952+Canician90@users.noreply.github.com>
2023-04-24 08:18:11 +0300

    hrht

--- starter/script.js
+++ starter/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 /*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -81,6 +80,16 @@ document.querySelector('.check').addEventListener('click', function () {
   //   }
   // }
 });
+
+
+/* 
+Implement a game rest functionality, so that the player can make a new guess! Here is how:
+1. Select the element with the 'again' class and attach a click event handler
+2. In the handler function, restore initial values of the score and secretNumber variables
+3. Restore the initial conditions of the message, number, score and guess input field
+4. Also restore the original background color (#222) and number width (15rem)
+
+
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
